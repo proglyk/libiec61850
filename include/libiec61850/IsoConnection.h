849056d701568b019624379e98de46b9565055d8
proglyk <ilyapronyashin23@yandex.ru>
2025-07-14 14:25:39 +0300

    Content of funcs 'IsoConnection_Create', 'IsoConnection_Delete' was updated

--- include/libiec61850/IsoConnection.h
+++ include/libiec61850/IsoConnection.h
@@ -1,7 +1,13 @@
 #ifndef _ISOCONNECTION_H_
 #define _ISOCONNECTION_H_
 
+#include "userint.h"
+
 struct sIsoConnection;
 typedef struct sIsoConnection *IsoConnectionPtr;
+
+IsoConnectionPtr IsoConnection_Create(s32_t);
+void	           IsoConnection_Delete(IsoConnectionPtr);
+void	           IsoConnection_ClientConnected(IsoConnectionPtr);
 
 #endif // _ISOCONNECTION_H_
--- src/libiec61850/IsoConnection.c
+++ src/libiec61850/IsoConnection.c
@@ -1,14 +1,49 @@
 #include "libiec61850/IsoConnection.h"
+#include "stdlib.h"
 
 struct sIsoConnection {
 	uint8_t* receive_buf;
-/* 	MessageReceivedHandler msgRcvdHandler;
-	void* msgRcvdHandlerParameter;
-	Socket2 socket;
-	int state;
+/*  	MessageReceivedHandler msgRcvdHandler;
+	void* msgRcvdHandlerParameter; */
+	s32_t socket;
+/*int state;
 	//CotpConnection * cotpConnection;
 	//void* session;
   //void* presentation;
 	//char* clientAddress;
 	Resources xLayer; */
 };
+
+/**	----------------------------------------------------------------------------
+	* @brief Connection init */
+IsoConnectionPtr
+	IsoConnection_Create(s32_t socket) {
+/*----------------------------------------------------------------------------*/
+	IsoConnectionPtr self = calloc(1, sizeof(struct sIsoConnection));
+	self->socket = socket;
+/*self->receive_buf = malloc(RECEIVE_BUF_SIZE);
+	self->msgRcvdHandler = NULL;
+	self->msgRcvdHandlerParameter = NULL;
+	self->state = ISO_CON_STATE_STOPPED;
+	SBuffer_Init(pxSbuf, p__buf1, SEND_BUF_SIZE); */
+	return self;
+}
+
+/**	----------------------------------------------------------------------------
+	* @brief Connection init */
+void
+	IsoConnection_Delete(IsoConnectionPtr self) {
+/*----------------------------------------------------------------------------*/
+	if (!self) return;
+	if (self->receive_buf)
+		free(self->receive_buf);
+	free(self);
+	self = NULL;
+}
+
+/**	----------------------------------------------------------------------------
+	* @brief Connection init */
+void
+	IsoConnection_ClientConnected(IsoConnectionPtr self) {
+/*----------------------------------------------------------------------------*/
+}
