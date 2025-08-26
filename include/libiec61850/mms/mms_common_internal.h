d221e86f5d0a7d35f20b525f6276ace3566f3738
proglyk <ilyapronyashin23@yandex.ru>
2025-08-26 13:12:15 +0300

    File 'mms_service_read.c' and related sources were added

--- include/libiec61850/mms/mms_common_internal.h
+++ include/libiec61850/mms/mms_common_internal.h
@@ -21,29 +21,28 @@
 	Globally viewed routines prototypes
 *******************************************************************************/
 
-
 MmsValue*
 mmsMsg_parseDataElement(Data_t* dataElement);
 
-// void
-// mmsMsg_createFloatData(MmsValue* value, int* size,  uint8_t** buf);
+void
+mmsMsg_createFloatData(MmsValue* value, int* size,  uint8_t** buf);
 
-// Data_t*
-// mmsMsg_createBasicDataElement(MmsValue* value);
+Data_t*
+mmsMsg_createBasicDataElement(MmsValue* value);
 
-// Data_t*
-// mmsMsg_createDataElement(MmsValue* value);
+Data_t*
+mmsMsg_createDataElement(MmsValue* value);
 
-// void
-// mmsMsg_addResultToResultList(AccessResult_t* accessResult, MmsValue* value);
+void
+mmsMsg_addResultToResultList(AccessResult_t* accessResult, MmsValue* value);
 
-// AccessResult_t**
-// mmsMsg_createAccessResultsList(MmsPdu_t* mmsPdu, int resultsCount);
+AccessResult_t**
+mmsMsg_createAccessResultsList(MmsPdu_t* mmsPdu, int resultsCount);
 
-// char*
-// mmsMsg_createStringFromAsnIdentifier(Identifier_t identifier);
+char*
+mmsMsg_createStringFromAsnIdentifier(Identifier_t identifier);
 
-// void
-// mmsMsg_deleteAccessResultList(AccessResult_t** accessResult, int variableCount);
+void
+mmsMsg_deleteAccessResultList(AccessResult_t** accessResult, int variableCount);
 
 #endif /*__MMS_COMMON_INTERNAL_H__*/
--- include/libiec61850/mms/mms_service_read.h
+++ include/libiec61850/mms/mms_service_read.h
@@ -9,40 +9,33 @@
 #define __MMS_SERVICE_READ_H__
 
 
-/*******************************************************************************
-	Defines
-*******************************************************************************/
-
-
-#include "libiec61850/mms/mms_server_conn.h" //#include "server/mms_server_connection.h"
-#include "libiec61850/utils/byte_buffer.h" //#include "server/byte_buffer.h"
+#include "libiec61850/mms/mms_server_conn.h"
+#include "libiec61850/utils/byte_buffer.h"
 #include "libiec61850/utils/linked_list.h"
 #include "iso9506/ReadRequest.h"
 #include "iso9506/AccessResult.h"
-
 
 
 /*******************************************************************************
 	Globally viewed routines prototypes
 *******************************************************************************/
 
-// typedef int MmsValue; //TODO
-// void appendValueToResultList(MmsValue* value, LinkedList values,
-	// AccessResult_t* accessResultList);
+
+void appendValueToResultList(MmsValue* value, LinkedList values,
+	AccessResult_t* accessResultList);
 
 
 void
 	mmsServer_handleReadRequest(MmsServerConnection* connection, ReadRequest_t* 
 		read, int invokeId,	ByteBuffer* response);
 		
-// char*
-	// createStringFromAsnIdentifier(Identifier_t identifier);
-
-// typedef int MmsTypeSpecification; //TODO
-// void
-	// addNamedVariableToResultList(MmsTypeSpecification* namedVariable, MmsDomain* 
-		// domain, char* nameIdStr, AccessResult_t* resultListEntry,	LinkedList values,
-			// MmsServerConnection* connection, AlternateAccess_t* alternateAccess);
+char*
+	createStringFromAsnIdentifier(Identifier_t identifier);
+	
+void
+	addNamedVariableToResultList(MmsTypeSpecification* namedVariable, MmsDomain* 
+		domain, char* nameIdStr, AccessResult_t* resultListEntry,	LinkedList values,
+			MmsServerConnection* connection, AlternateAccess_t* alternateAccess);
 
 
 #endif /*__MMS_SERVICE_READ_H__*/
